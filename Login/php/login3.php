e362a89ce38696ec4b4482183ebb619b31acffe7
amyn-ali1 <amynali269@gmail.com>
2022-06-26 21:29:10 +0100

    add session in log in and add hash to groupe

--- Login/php/login3.php
+++ Login/php/login3.php
@@ -32,28 +32,28 @@ if(isset($_POST['username']) &&
 
             if(password_verify($passwd, $donnees['passwd'])){
             if($donnees['statut'] == "Encadrant"){
-                $_SESSION['id_compte'] = $id;
-                 $_SESSION['statut'] = $statut;
+                $_SESSION['id_compte'] = $donnees['id_compte'];
+                $_SESSION['statut'] = $donnees['status'];
                 //header encadrant page
                  header("Location: ../../Encadrant/Main/main.php");
                  
                  exit;
-                }}
-
-               
+                }
+       
             if($donnees['statut'] == "Groupe"){
-                $_SESSION['id_compte'] = $id;
-                 $_SESSION['statut'] = $statut;
+                $_SESSION['id_compte'] = $donnees['id_compte'];
+                $_SESSION['statut'] = $donnees['status'];
                 //header groupe page
                  header("Location: ../../Etudiant/Main/main.php");
                  
                  exit;
-                }
+               }
+           }  
             //admin with out hash
             if($passwd == $donnees['passwd']){ 
             if($donnees['statut'] == "Admin"){
-                $_SESSION['id_compte'] = $id;
-                 $_SESSION['statut'] = $statut;
+                $_SESSION['id_compte'] = $donnees['id_compte'];
+                $_SESSION['statut'] = $donnees['status'];
                 //header admin page
                  header("Location: ../../Admin/Main/main.php");
                  
